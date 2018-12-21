d9a3456abd771ead5acbd06b3b12650481115834
SomaC <SomaCruz@bk.ru>
2018-12-21 23:15:19 +0300

    Added setup.py and updated gitignore

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+setup(
+    name='ITMO_FS',
+    version='0.01',
+    packages=find_packages(),
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
+)
