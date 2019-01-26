ab0e8506a3b07b2db237649df0937d11e2a428da
Shivam Mehta <shivam.mehta007@gmail.com>
2019-01-26 15:04:04 +0530

    Adding the limit to number of selecting feature

--- wrappers/SequentialForwardSelection.py
+++ wrappers/SequentialForwardSelection.py
@@ -30,9 +30,9 @@ class SequentialForwardSelection:
         """
 
     def __init__(self, estimator, n_features, seed=1):
-        self._estimator_ = estimator
-        self.n_features_ = n_features
-        self.features_ = []
+        self.__estimator__ = estimator
+        self.__n_features__ = n_features
+        self.__features__ = []
         rnd.seed(seed)
 
     def fit(self, X, y, test_x, test_y):
@@ -65,15 +65,19 @@ class SequentialForwardSelection:
         for feature in X[0:1,:]:
             old_features = current_features
             current_features = np.append(current_features, feature)
-            self._estimator_.fit([X[i] for i in current_features], y)
+            self.__estimator__.fit([X[i] for i in current_features], y)
 
             current_accuracy = self.get_current_accuracy(X, current_features, test_x, test_y)
 
             if current_accuracy > accuracy :
-                self.features_ = current_features
+                self.__features__ = current_features
                 accuracy = current_accuracy
+                if len(self.__features__) == self.__n_features__:
+                    break
             else:
                 current_features = old_features
+
+
 
     def get_current_accuracy(self, X, current_features, test_x, test_y):
         '''
@@ -96,11 +100,11 @@ class SequentialForwardSelection:
 
         correct = 0
         for i in range(test_x.length):
-            predict = self._estimator_.predict([X[j] for j in current_features])
+            predict = self.__estimator__.predict([X[j] for j in current_features])
             if predict == test_y[i]:
                 correct += 1
         current_accuracy = correct / test_x.length
         return current_accuracy
 
     def predict(self, X):
-        self._estimator_.predict([X[i] for i in self.features_])
+        self.__estimator__.predict([X[i] for i in self.__features__])
