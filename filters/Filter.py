54d30b8b0d020e7c7dc82e955365d0e963d43cc0
SomaC <SomaCruz@bk.ru>
2019-09-06 16:14:36 +0300

    added normalize function

--- filters/Filter.py
+++ filters/Filter.py
@@ -1,4 +1,10 @@
+from functools import partial
+
+import numpy as np
+
 import filters
+
+
 # TODO: move all feature_names?
 
 
@@ -20,14 +26,61 @@ class DefaultMeasures:
     VDM = filters.VDM()  # TODO: probably not a filter
 
 
-print(DefaultMeasures.SpearmanCorrelation)
+def spearman_corr(x, y):
+    n = x.shape[0]
+    c = 6 / (n * (n - 1) * (n + 1))
 
-GLOB_MEASURE = {"FitCriterion": DefaultMeasures.FitCriterion}
+    dif = x - np.vstack(tuple([y] * x.shape[1])).T
+    return 1 - c * np.sum(dif * dif, axis=0)
 
-GLOB_CR = {}
+
+def pearson_corr(x, y):
+    x_dev = x - np.mean(x, axis=0)
+    y_dev = y - np.mean(y)
+    sum_dev = y_dev.dot(x_dev)
+    sq_dev_x = x_dev * x_dev
+    sq_dev_y = y_dev * y_dev
+    return sum_dev / np.sqrt(np.sum(sq_dev_y) * np.sum(sq_dev_x))
+
+
+
+
+# print(DefaultMeasures.SpearmanCorrelation)
+
+GLOB_MEASURE = {"FitCriterion": DefaultMeasures.FitCriterion, "SpearmanCorr": spearman_corr,
+                "PearsonCorr": pearson_corr}
+
+GLOB_CR = {"Best by value": select_best_by_value, "Worst by value": select_worst_by_value}
+
 
 class DefaultCuttingRules:
-    pass
+
+    def select_best_by_value(value):
+        return partial(_select_by_value, value=value, more=True)
+
+    def select_worst_by_value(value):
+        return partial(_select_by_value, value=value, more=False)
+
+    def __select_by_value(scores, value, more=True):
+        features = []
+        for key, sc_value in scores.items():
+            if more:
+                if sc_value >= value:
+                    features.append(key)
+            else:
+                if sc_value <= value:
+                    features.append(key)
+        return features
+
+    def select_k_best(k):
+        return partial(self.__select_k, k=k)
+
+    def select_k_worst(k):
+        return partial(__select_k, k=-k)
+
+    def __select_k(scores, k):
+        return [keys[0] for keys in sorted(scores.items(), key=lambda kv: kv[1])[:k]]
+
 
 class Filter:
     def __init__(self, measure, cutting_rule):
@@ -37,12 +90,8 @@ class Filter:
 
     def run(self, x, y, store_scores=False):
         self.feature_scores = None
-        feature_scores = self.measure.run(x, y)
+        feature_scores = self.measure(x, y)
         if store_scores:
             self.feature_scores = feature_scores
         selected_features = self.cutting_rule(feature_scores)
-        #selected_features
-
-
-
-
+        return x[selected_features]
