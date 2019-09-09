bfd4dee0e4826a8ca18ae18a7c6597a2d285a218
Paul Khuzhin <poul1298@gmail.com>
2019-09-09 03:27:38 +0300

    Added InformationGain measure

--- filters/Filter.py
+++ filters/Filter.py
@@ -1,6 +1,7 @@
 from functools import partial
 
 import numpy as np
+import math
 
 import filters
 
@@ -15,6 +16,7 @@ import filters
 #               1,
 #               0,
 #               0])
+
 # {0: 0.75, 1: 0.75, 2: 0.5, 3: 1.0, 4: 0.75}
 
 # Default measures
@@ -99,8 +101,53 @@ class DefaultMeasures:
         return np.abs(1 - np.sum(np.multiply(diff_x.T, diff_y).T, axis=0))
 
     # IGFilter = filters.IGFilter()  # TODO: unexpected .run() interface; .run() feature_names; no default constructor
-    # @staticmethod
-    # def ig_measure(X, y):
+
+    # return array(ratio)
+    @staticmethod
+    def ig_measure(X, y):
+        # Calculate the entropy of y.
+        dict_label = dict()
+        for label in y:
+            if label not in dict_label:
+                dict_label.update({label: 1})
+            else:
+                dict_label[label] += 1
+        entropy = 0.0
+        for i in dict_label.values():
+            entropy += -i / len(y) * math.log(i / len(y), 2)
+
+        list_f = np.empty(X.shape[1])
+        for index in range(len(X.T)):
+            dict_i = dict()
+            for i in range(len(X.T[index])):
+                if X.T[index][i] not in dict_i:
+                    dict_i.update({X.T[index][i]: [i]})
+                else:
+                    dict_i[X.T[index][i]].append(i)
+            # print(dict_i)
+
+            # Conditional entropy of a feature.
+            con_entropy = 0.0
+            # get corresponding values in y.
+            for f in dict_i.values():
+                # Probability of each class in a feature.
+                p = len(f) / len(X.T[0])
+                # Dictionary of corresponding probability in labels.
+                dict_y = dict()
+                for i in f:
+                    if y[i] not in dict_y:
+                        dict_y.update({y[i]: 1})
+                    else:
+                        dict_y[y[i]] += 1
+
+                # calculate the probability of corresponding label.
+                sub_entropy = 0.0
+                for l in dict_y.values():
+                    sub_entropy += -l / sum(dict_y.values()) * math.log(l / sum(dict_y.values()), 2)
+
+                con_entropy += sub_entropy * p
+            list_f[index] = entropy - con_entropy
+        return list_f
 
     # RandomFilter = filters.RandomFilter() # TODO: bad .run() interface; .run() feature_names; no default constructor
 
