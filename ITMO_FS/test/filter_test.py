6ad8e5ed446c2d2b5ce6e3cf6d0819a5589dd436
SomaC <SomaCruz@bk.ru>
2019-12-18 13:06:49 +0300

    restructuring and workflow stabilizing

--- ITMO_FS/test/filter_test.py
+++ ITMO_FS/test/filter_test.py
@@ -1,5 +1,3 @@
-import os
-import sys
 import time
 import unittest
 
@@ -10,15 +8,15 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import f1_score
 from sklearn.svm import SVC
 
-sys.path.append(os.path.abspath(os.path.join('..\\..', 'ITMO_FS')))
-
-from ITMO_FS.filters.Filter import *
-from ITMO_FS.hybrid.Melif import Melif
-from ITMO_FS.wrappers.AddDelWrapper import *
-from ITMO_FS.wrappers.BackwardSelection import *
+from ..filters.Filter import *
+from ..hybrid.Melif import Melif
+from ..wrappers.AddDelWrapper import *
+from ..wrappers.BackwardSelection import *
 
 
-class MyTestCase(unittest.TestCase):
+
+
+class TestCases(unittest.TestCase):
     basehock = scipy.io.loadmat('BASEHOCK.mat')
     coil = scipy.io.loadmat('COIL20.mat')
     orl = scipy.io.loadmat('orlraws10P.mat')
