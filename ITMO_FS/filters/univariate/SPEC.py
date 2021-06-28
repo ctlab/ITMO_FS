9dc24fe746e6d067816ca498f113d259ca45e411
MadHypnofrog <madhypnofrog@gmail.com>
2021-06-28 06:49:16 +0300

    Add logging to univariate filters

--- ITMO_FS/filters/univariate/SPEC.py
+++ ITMO_FS/filters/univariate/SPEC.py
@@ -1,49 +1,49 @@
+from logging import getLogger
+
 import numpy as np
 from scipy.linalg import eigh
-from ...utils import l21_norm, matrix_norm, power_neg_half, BaseTransformer
 from sklearn.metrics.pairwise import pairwise_distances
+
+from ...utils import l21_norm, matrix_norm, power_neg_half, BaseTransformer
 
 
 class SPEC(BaseTransformer):
+    """Spectral Feature Selection algorithm.
+
+    Parameters
+    ----------
+    n_features : int
+        Number of features to select.
+    k : int
+        Amount of clusters to find.
+    gamma : callable
+        An "increasing function that penalizes high frequency components".
+        Default is gamma(x) = x^2.
+    sigma : float
+        Parameter for the weighting scheme.
+    phi_type : int (1, 2 or 3)
+        Type of feature ranking function to use.
+
+    Notes
+    -----
+    For more details see `this paper <http://www.public.asu.edu/~huanliu/papers/icml07.pdf/>`_.
+
+    Examples
+    --------
+    >>> from ITMO_FS.filters.univariate import SPEC
+    >>> import numpy as np
+    >>> X = np.array([[1, 2, 3, 3, 1], [2, 2, 3, 3, 2], [1, 3, 3, 1, 3],
+    ... [1, 1, 3, 1, 4], [2, 4, 3, 1, 5]])
+    >>> y = np.array([1, 2, 1, 1, 2])
+    >>> model = SPEC(3).fit(X, y)
+    >>> model.selected_features_
+    array([0, 1, 4], dtype=int64)
+    >>> model = SPEC(3).fit(X)
+    >>> model.selected_features_
+    array([3, 4, 1], dtype=int64)
     """
-        Performs the Spectral Feature Selection algorithm.
-
-        Parameters
-        ----------
-        n_features : int
-            Number of features to select.
-        k : int
-            Amount of clusters to find.
-        gamma : callable
-            An "increasing function that penalizes high frequency components".
-            Default is gamma(x) = x^2.
-        sigma : float
-            Parameter for the weighting scheme.
-        phi_type : int (1, 2 or 3)
-            Type of feature ranking function to use.
-
-        Notes
-        -----
-        For more details see `this paper <http://www.public.asu.edu/~huanliu/papers/icml07.pdf/>`_.
-
-
-        Examples
-        --------
-        >>> from ITMO_FS.filters.univariate import SPEC
-        >>> import numpy as np
-        >>> X = np.array([[1, 2, 3, 3, 1], [2, 2, 3, 3, 2], [1, 3, 3, 1, 3], \
-[1, 1, 3, 1, 4], [2, 4, 3, 1, 5]])
-        >>> y = np.array([1, 2, 1, 1, 2])
-        >>> model = SPEC(3).fit(X, y)
-        >>> model.selected_features_
-        array([0, 1, 4], dtype=int64)
-        >>> model = SPEC(3).fit(X)
-        >>> model.selected_features_
-        array([3, 4, 1], dtype=int64)
-    """
-
     def __init__(self, n_features, k=2, gamma=(lambda x: x ** 2), sigma=0.5,
-            phi_type=3):
+                 phi_type=3):
         self.n_features = n_features
         self.k = k
         self.gamma = gamma
@@ -57,37 +57,36 @@ class SPEC(BaseTransformer):
         return np.sum(cosines * cosines * self.gamma(eigvals))
 
     def __phi2(self, cosines, eigvals, k):
-        return (np.sum(cosines[1:] * cosines[1:] * self.gamma(eigvals[1:])) /
-        np.sum(cosines[1:] * cosines[1:]))
+        return (np.sum(cosines[1:] * cosines[1:] * self.gamma(eigvals[1:]))
+                / np.sum(cosines[1:] * cosines[1:]))
 
     def __phi3(self, cosines, eigvals, k):
-        return np.sum(cosines[1:k] * cosines[1:k] * (self.gamma(2) -
-            self.gamma(eigvals[1:k])))
+        return np.sum(cosines[1:k] * cosines[1:k]
+                      * (self.gamma(2) - self.gamma(eigvals[1:k])))
 
     def _fit(self, X, y):
+        """Fit the filter.
+
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_features)
+            The training input samples.
+        y : array-like, shape (n_samples,), optional
+            The target values. If present, label values are used to
+            construct the similarity graph and the amount of classes
+            overrides k.
+
+        Returns
+        -------
+        None
         """
-            Fits the filter.
-
-            Parameters
-            ----------
-            X : array-like, shape (n_samples, n_features)
-                The training input samples.
-            y : array-like
-                The target values. If present, label values are used to
-                construct the similarity graph and the amount of classes
-                overrides k.
-
-            Returns
-            ----------
-            None
-        """
-
         def calc_weight(f):
             f_norm = np.sqrt(D).dot(f)
             f_norm /= np.linalg.norm(f_norm)
 
-            cosines = np.apply_along_axis(lambda vec: np.dot(vec /
-                np.linalg.norm(vec), f_norm), 0, eigvectors)
+            cosines = np.apply_along_axis(
+                lambda vec: np.dot(vec / np.linalg.norm(vec), f_norm), 0,
+                eigvectors)
             return phi(cosines, eigvals, k)
 
         if self.phi_type == 1:
@@ -97,37 +96,50 @@ class SPEC(BaseTransformer):
         elif self.phi_type == 3:
             phi = self.__phi3
         else:
-            raise ValueError("phi_type should be 1, 2 or 3, %d passed" %
-                self.phi_type)
+            getLogger(__name__).error(
+                "phi_type should be 1, 2 or 3, %d passed", self.phi_type)
+            raise ValueError(
+                "phi_type should be 1, 2 or 3, %d passed" % self.phi_type)
 
         n_samples = X.shape[0]
 
-        if self.n_features > self.n_features_:
-            raise ValueError("Cannot select %d features with n_features = %d" %
-                (self.n_features, self.n_features_))
-
         if y is None:
             if self.k > n_samples:
-                raise ValueError("Cannot find %d clusters with n_samples = %d" %
-                    (self.k, n_samples))
+                getLogger(__name__).error(
+                    "Cannot find %d clusters with n_samples = %d",
+                    self.k, n_samples)
+                raise ValueError(
+                    "Cannot find %d clusters with n_samples = %d"
+                    % (self.k, n_samples))
             k = self.k
             graph = np.ones((n_samples, n_samples))
-            W = graph * pairwise_distances(X, metric=lambda x, y:
-                    self.__scheme(x, y))
+            W = graph * pairwise_distances(
+                X, metric=lambda x, y: self.__scheme(x, y))
         else:
             values, counts = np.unique(y, return_counts=True)
             values_dict = dict(zip(values, counts))
             k = len(values)
-            W = pairwise_distances(y.reshape(-1, 1), metric=lambda x, y:
-                (x[0] == y[0]) / values_dict[x[0]])
+            W = pairwise_distances(
+                y.reshape(-1, 1),
+                metric=lambda x, y: (x[0] == y[0]) / values_dict[x[0]])
+
+        getLogger(__name__).info("W: %s", W)
 
         D = np.diag(W.sum(axis=1))
+        getLogger(__name__).info("D: %s", D)
         L = D - W
+        getLogger(__name__).info("L: %s", L)
         L_norm = power_neg_half(D).dot(L).dot(power_neg_half(D))
+        getLogger(__name__).info("Normalized L: %s", L_norm)
         eigvals, eigvectors = eigh(a=L_norm)
+        getLogger(__name__).info(
+            "Eigenvalues for normalized L: %s, eigenvectors: %s",
+            eigvals, eigvectors)
 
-        weights = np.apply_along_axis(lambda f: calc_weight(f), 0, X)
-        ranking = np.argsort(weights)
+        self.feature_scores_ = np.apply_along_axis(
+            lambda f: calc_weight(f), 0, X)
+        getLogger(__name__).info("Feature scores: %s", self.feature_scores_)
+        ranking = np.argsort(self.feature_scores_)
         if self.phi_type == 3:
             ranking = ranking[::-1]
         self.selected_features_ = ranking[:self.n_features]
--- ITMO_FS/filters/univariate/UnivariateFilter.py
+++ ITMO_FS/filters/univariate/UnivariateFilter.py
@@ -1,49 +1,50 @@
-from .measures import CR_NAMES, MEASURE_NAMES
+from logging import getLogger
+
 import numpy as np
-from ...utils import BaseTransformer, generate_features, check_restrictions, \
-    apply_cr
+
+from .measures import CR_NAMES, MEASURE_NAMES
+from ...utils import (BaseTransformer, generate_features, check_restrictions,
+                      apply_cr)
 
 
-class UnivariateFilter(BaseTransformer):  # TODO ADD LOGGING
+class UnivariateFilter(BaseTransformer):
+    """Basic interface for using univariate measures for feature selection.
+    List of available measures is in ITMO_FS.filters.univariate.measures, also
+    you can provide your own measure but it should suit the argument scheme for
+    measures, i.e. take two arguments x,y and return scores for all the
+    features in dataset x. Same applies to cutting rules.
+
+    Parameters
+    ----------
+    measure : string or callable
+        A metric name defined in GLOB_MEASURE or a callable with signature
+        measure (sample dataset, labels of dataset samples) which should
+        return a list of metric values for each feature in the dataset.
+    cutting_rule : string or callables
+        A cutting rule name defined in GLOB_CR or a callable with signature
+        cutting_rule (features) which should return a list of features ranked by
+        some rule.
+
+    See Also
+    --------
+
+    Examples
+    --------
+
+    >>> import numpy as np
+    >>> from ITMO_FS.filters.univariate import select_k_best
+    >>> from ITMO_FS.filters.univariate import UnivariateFilter
+    >>> from ITMO_FS.filters.univariate import f_ratio_measure
+    >>> x = np.array([[3, 3, 3, 2, 2], [3, 3, 1, 2, 3], [1, 3, 5, 1, 1],
+    ... [3, 1, 4, 3, 1], [3, 1, 2, 3, 1]])
+    >>> y = np.array([1, 3, 2, 1, 2])
+    >>> filter = UnivariateFilter(f_ratio_measure,
+    ... select_k_best(2)).fit(x, y)
+    >>> filter.selected_features_
+    array([4, 2], dtype=int64)
+    >>> filter.feature_scores_
+    array([0.6 , 0.2 , 1.  , 0.12, 5.4 ])
     """
-        Basic interface for using univariate measures for feature selection.
-        List of available measures is in ITMO_FS.filters.univariate.measures,
-        also you can provide your own measure but it should suit the argument
-        scheme for measures, i.e. take two arguments x,y and return scores for
-        all the features in dataset x. Same applies to cutting rules.
-
-        Parameters
-        ----------
-        measure : string or callable
-            A metric name defined in GLOB_MEASURE or a callable with signature
-            measure (sample dataset, labels of dataset samples) which should
-            return a list of metric values for each feature in the dataset.
-        cutting_rule : string or callables
-            A cutting rule name defined in GLOB_CR or a callable with signature
-            cutting_rule (features),
-            which should return a list features ranked by some rule.
-
-        See Also
-        --------
-
-        Examples
-        --------
-
-        >>> import numpy as np
-        >>> from ITMO_FS.filters.univariate import select_k_best
-        >>> from ITMO_FS.filters.univariate import UnivariateFilter
-        >>> from ITMO_FS.filters.univariate import f_ratio_measure
-        >>> x = np.array([[3, 3, 3, 2, 2], [3, 3, 1, 2, 3], [1, 3, 5, 1, 1], \
-[3, 1, 4, 3, 1], [3, 1, 2, 3, 1]])
-        >>> y = np.array([1, 3, 2, 1, 2])
-        >>> filter = UnivariateFilter(f_ratio_measure,
-        ... select_k_best(2)).fit(x, y)
-        >>> filter.selected_features_
-        array([4, 2], dtype=int64)
-        >>> filter.feature_scores_
-        array([0.6 , 0.2 , 1.  , 0.12, 5.4 ])
-    """
-
     def __init__(self, measure, cutting_rule=("Best by percentage", 1.0)):
         self.measure = measure
         self.cutting_rule = cutting_rule
@@ -53,39 +54,44 @@ class UnivariateFilter(BaseTransformer):  # TODO ADD LOGGING
             try:
                 measure = MEASURE_NAMES[self.measure]
             except KeyError:
-                raise KeyError("No %r measure yet" % self.measure)
+                getLogger(__name__).error("No %s measure yet", self.measure)
+                raise KeyError("No %s measure yet" % self.measure)
         elif hasattr(self.measure, '__call__'):
             measure = self.measure
         else:
+            getLogger(__name__).error(
+                "%s isn't a measure function or string", self.measure)
             raise KeyError(
-                "%r isn't a measure function or string" % self.measure)
+                "%s isn't a measure function or string" % self.measure)
         return measure
 
     def _fit(self, X, y, store_scores=True):
+        """Fit the filter.
+
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_features)
+            The training input samples.
+        y : array-like, shape (n_samples,)
+            The target values.
+        store_scores : boolean, optional
+            In case you want to store the scores of features
+            for future calls to Univariate filter; default True
+
+        Returns
+        -------
+        None
         """
-            Fits the filter.
-
-            Parameters
-            ----------
-            X : array-like, shape (n_samples, n_features)
-                The training input samples.
-            y : array-like, shape (n_samples, )
-                The target values.
-            store_scores : boolean, optional
-                In case you want to store the scores of features
-                for future calls to Univariate filter; default False
-
-            Returns
-            ------
-            None
-        """
-
         measure = self.__apply_ms()
         cutting_rule = apply_cr(self.cutting_rule)
+        getLogger(__name__).info(
+            "Using UnivariateFilter with measure %s and cutting rule %s",
+            measure, cutting_rule)
 
         check_restrictions(measure.__name__, cutting_rule.__name__)
 
         feature_scores = measure(X, y)
+        getLogger(__name__).info("Feature scores: %s", feature_scores)
 
         if store_scores:
             self.feature_scores_ = feature_scores
